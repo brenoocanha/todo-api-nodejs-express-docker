6d7b2b40dcb40306685c00f5cbc32bea1350df27
Breno Ocanha <breno@dmxdesign.com.br>
2023-08-26 21:27:52 -0300

    feat: env + controllers base structure

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.status(200).send('Router works!'));
+router.get('/tasks');
 
 module.exports = router;
